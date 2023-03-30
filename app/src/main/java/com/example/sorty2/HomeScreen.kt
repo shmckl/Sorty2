a78606a7dbc89693dbc27e34bb70c22713d02937
Dmitriy Pak <dimapak75@gmail.com>
2023-03-30 15:30:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sorty2/HomeScreen.kt
@@ -0,0 +1,69 @@
+package com.example.sorty2
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.sorty2.ui.theme.Sorty2Theme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen() {
+    Sorty2Theme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Scaffold(
+                floatingActionButton = {
+                    FloatingActionButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onPrimaryContainer
+                        )
+                    }
+                },
+                topBar = {
+                    SmallTopAppBar(
+                        title = {
+                            Text(text = "Material 3")
+                        },
+                        colors = TopAppBarDefaults.smallTopAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                            titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    )
+                }
+            ) { values ->
+                LazyColumn(contentPadding = values) {
+                    items(20) {
+                        ImageCard(
+                            title = "Bacon ipsum",
+                            description = "Bacon ipsum dolor amet pork shankle beef andouille ball tip. Meatball corned beef swine, strip steak bacon jerky doner tongue biltong pork loin drumstick sausage hamburger burgdoggen.",
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+@Preview(
+    name = "Home Screen",
+    showBackground = true
+)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
